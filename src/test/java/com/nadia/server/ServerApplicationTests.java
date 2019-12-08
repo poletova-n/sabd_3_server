38e8601d7e30d73af004dfbf75dc99e0106a5099
nadia <poletova-n@mail.ru>
2019-12-09 01:37:12 +0300

    first commit

---
+++ src/test/java/com/nadia/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadia.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
